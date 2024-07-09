b68540fa95d0f7ddb8e42149556ab8b84a2f65c9
chrialge <chrialge99@gmail.com>
2024-07-09 20:19:16 +0200

    add header, footer, jumbotron, banner_date, category and who_are

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,106 @@ export default {
 <template>
   <AppHeader />
 
+  <main id="site_main">
+    <section id="jumbotron">
+      <div class="container jumbo_container">
+        <h2>Corso di clown</h2>
+        <p>
+          Stai cercando una lavoro che ti porta a girare tutta l'europa e conoscere persone variegate, sei nel posto
+          giusto!
+        </p>
+        <div class="search_container">
+          <input type="search" name="text_user" id="text_user" placeholder="search">
+          <button class="search_btn">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </button>
+
+        </div>
+      </div>
+
+
+    </section>
+
+
+    <section id="banner_data">
+      <div class="container container_data">
+        <div class="banner_single">
+          <h4>100+</h4>
+          <span>MAGHI DEL CODICE</span>
+        </div>
+
+        <div class="banner_single">
+          <h4>100+</h4>
+          <span>MAGHI DEL CODICE</span>
+        </div>
+
+        <div class="banner_single">
+          <h4>100+</h4>
+          <span>MAGHI DEL CODICE</span>
+        </div>
+
+        <div class="banner_single">
+          <h4>100+</h4>
+          <span>MAGHI DEL CODICE</span>
+        </div>
+      </div>
+
+    </section>
+
+    <section id="who_are">
+      <div class="container container_who_are">
+        <div class="who_are_image">
+
+        </div>
+        <div class="who_are_info">
+          <span>Scopri di piu su di noi</span>
+          <h2>Chi Siamo</h2>
+          <p>
+            Benvenuti nel nostro magico mondo dove potresti trovare la tua strada! Offriamo corsi di circense,
+            trapezista,
+            giochelliere, domatore e sopratutto la nostra rinomata scuola di pagliacci. Interattivita nelle lezioni
+            esperti nel settore del circo europeo.
+          </p>
+          <ul>
+            <li>
+              Esperti preparatori atletici e fisoterapisti per prepararvi atleticamente
+            </li>
+            <li>
+              Corsi specifici ad singolo ruolo, coinvolgente per aiutare l'appredimento e la sicurezza
+            </li>
+            <li>
+              Abbiamo vinto per la miglior scuola di pagliacci.
+            </li>
+          </ul>
+          <button class="btn_primary">
+            SCOPRI DI PIU
+          </button>
+        </div>
+      </div>
+
+    </section>
+
+
+    <section id="category">
+      <div class="container container_category">
+        <span>Scegli la categoria di corsi dalla quale vuoi iniziare</span>
+        <h3>Categorie Popolari</h3>
+        <button class="btn_primary">
+          GUARDA TUTTO
+        </button>
+        <div class="categories_container">
+          <div class="category_card">
+            <div class="category_icon">
+              
+            </div>
+            <div class="category_info"></div>
+          </div>
+        </div>
+      </div>
+
+    </section>
+  </main>
+
   <AppFooter />
 
 
--- src/components/partials/AppFooter.vue
+++ src/components/partials/AppFooter.vue
@@ -11,7 +11,140 @@ export default {
 
 <template>
 
-    ciao
+    <footer id="site_footer">
+        <div class="footer_top">
+            <div class="container">
+                <div class="social">
+                    <div class="logo">
+                        lorem
+                    </div>
+                    <div class="widget_social">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <i class="fa-brands fa-facebook"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa-brands fa-twitter"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa-brands fa-instagram"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa-brands fa-linkedin"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa-brands fa-youtube"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="connect">
+                    <h4>Collegamenti</h4>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Chi siamo
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Corsi
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Contatti
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="about">
+                    <h4>Collegamenti</h4>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Come funziona
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Prezzi
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Promozioni
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="help_center">
+                    <h4>Collegamenti</h4>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Pagamenti
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                FAQs
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Checkout
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Other
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="contacts">
+                    <h4>Contatti</h4>
+                    <ul>
+                        <li>
+                            <span>contact@corsocircense.it</span>
+
+                        </li>
+                        <li>
+                            <span>
+                                + 110 1000 0111
+                            </span>
+                        </li>
+                        <li>
+                            <span>
+                                Rimini, IT
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer_bottom">
+            <div class="copyright">
+                &copy; 2024 Christian Algieri. All rights reserved
+            </div>
+        </div>
+    </footer>
 
 </template>
 
